edb7c5ba0a615bab7990fe458ba2b518d6eeeaaf
Vinilimm <viniciuscarneiro.1001@hotmail.com>
2024-08-28 22:57:49 -0300

    primeiro get pronto

--- src/main/java/com/example/WebStream/controllers/FilmeController.java
+++ src/main/java/com/example/WebStream/controllers/FilmeController.java
@@ -1,10 +1,25 @@
 package com.example.WebStream.controllers;
 
 
+import com.example.WebStream.models.Filme;
+import com.example.WebStream.services.FilmeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
 
+@RestController
+@RequestMapping("/api/filmes")
 public class FilmeController {
+
+    @Autowired
+    private FilmeService filmeService;
+
+    @GetMapping
+    public List<Filme> getAllTopicos() {
+        return filmeService.listarfilmes();
+    }
 
 }
--- src/main/java/com/example/WebStream/services/FilmeService.java
+++ src/main/java/com/example/WebStream/services/FilmeService.java
@@ -1,8 +1,26 @@
 package com.example.WebStream.services;
 
+import com.example.WebStream.models.Filme;
+import com.example.WebStream.repository.FilmeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FilmeService {
 
+    @Autowired
+    private FilmeRepository filmeRepository;
+
+
+
+
+
+
+
+
+    public List<Filme> listarfilmes() {
+        return filmeRepository.findAll();
+    }
 }
